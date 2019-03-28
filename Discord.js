434029ff2aa66acc3e0898c9ea5e888306ff008e
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-03-28 19:53:37 +0900

    fix

--- Discord.js
+++ Discord.js
@@ -7,39 +7,40 @@ var config = JSON.parse(fs.readFileSync("DiscordConfig.json","utf8")) ;
 const client = new Discord.Client();    
 const Token = config["Connect"]["token"];
 const Path = config["Image"]["Path"];
-const Latest = config["Image"]["Latest"];
+var latest = config["Image"]["Latest"];
 
 console.log(Token)
 console.log(Path)
-console.log(Latest)
+console.log(latest)
+
+//Get Image list
+image = fs.readdirSync(Path)
+//image_l = image.length
+console.log(image)
 
 client.on("ready", ()=> {
     console.log("ready...");
 });
 
 client.on("message",message => {
-    if(message.author.bot){
-        return;
-    }else{
-        let msgc = message.content;
-        let channel = message.channel;
-        let author = message.author.username;
-		//msgを送信します
+   let msgc = message.content;
+    let channel = message.channel;
+    let author = message.author.username;
+	//msgを送信します
 		
-		if(msgc.toUpperCase() === 'Reload'.toUpperCase()){//keyword file reload
-			message.reply("KeyWord File Reload")
-				.then(message => console.log("KeyWord File Reload"))
-				.catch(console.error);
-			KeyWord = JSON.parse(fs.readFileSync("KeyWord.json","utf8")) ;
-			return;			
-		}else{
-				.then(message => console.log(`Sent message: ${msgc}`))
-				.catch(console.error);
-			return;
-		}
-   }
-}
-);
-
+	if(msgc === 'ようこそ'){//keyword file reload
+		var s_image = image[Math.floor(Math.random() * (image.length))]
+		message.reply("ようこそ",{files:['Image/'+ s_image]})
+		.then(message => console.log("youkoso"))
+		.catch(console.error);
+		latest = s_image 
+		return;			
+	}else if(msgc === '!画像追加'){
+		image = fs.readdirSync(Path)	
+		message.reply("画像一覧を再読込しました。\n" + "現在の画像数:"+image.length+"枚")	
+			.then(message => console.log(`Sent message: ${msgc}`))
+			.catch(console.error);
+		return;
+	}
+});
 client.login(Token);
-
